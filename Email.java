cc1bda0d56f51f76dbf5dcac11353df65ce1087c
Leon Joel <leonjoelquainoo@gmail.com>
2022-11-16 21:42:10 -0500

    Update Email.java

--- Email.java
+++ Email.java
@@ -18,98 +18,51 @@ public class Email implements Serializable{
 		this.cc = "";
 		this.bcc = "";
 	}
-
-	/**
-	 * @return the to
-	 */
+	
 	public String getTo() {
 		return to;
 	}
-
-
-	/**
-	 * @param to the to to set
-	 */
+	
 	public void setTo(String to) {
 		this.to = to;
 	}
-
-
-	/**
-	 * @return the cc
-	 */
+	
 	public String getCc() {
 		return cc;
 	}
 
-
-	/**
-	 * @param cc the cc to set
-	 */
 	public void setCc(String cc) {
 		this.cc = cc;
 	}
 
-
-	/**
-	 * @return the bcc
-	 */
 	public String getBcc() {
 		return bcc;
 	}
-
-
-	/**
-	 * @param bcc the bcc to set
-	 */
+	
 	public void setBcc(String bcc) {
 		this.bcc = bcc;
 	}
-
-
-	/**
-	 * @return the subject
-	 */
+	
 	public String getSubject() {
 		return subject;
 	}
-
-
-	/**
-	 * @param subject the subject to set
-	 */
+	
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
-
-
-	/**
-	 * @return the body
-	 */
+	
 	public String getBody() {
 		return body;
 	}
-
-
-	/**
-	 * @param body the body to set
-	 */
+	
 	public void setBody(String body) {
 		this.body = body;
 	}
-
-
-	/**
-	 * @return the timestamp
-	 */
+	
 	public GregorianCalendar getTimestamp() {
 		return timestamp;
 	}
-
-
-	/**
-	 * @param timestamp the timestamp to set
-	 */
+	
 	public void setTimestamp(GregorianCalendar timestamp) {
 		this.timestamp = timestamp;
 	}
